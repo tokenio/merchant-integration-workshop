89972cf68db40529027066e297f814d6b95a68ce
Harry Jian <harry@token.io>
2019-01-31 18:57:21 -0800

    Fixes

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+// dev key 4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
 app.use(
     express.json(),
-    express.static(__dirname)
+    express.static(__dirname),
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {maxAge: 60000}
+    })
 );
 
 app.listen(3000, function () {
